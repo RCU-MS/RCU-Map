3bdbfd524f06045b9830277477c02eac0306a78e
RCU-MS <justinrcu@gmail.com>
2015-08-18 17:10:19 -0500

    Commit #1
    Commit #1

--- js/app.js
+++ js/app.js
@@ -1,28 +1,21 @@
+
 //Global used to represent the current selection being chosen
 var current;
 
-//Highlight selection for the first selection options
+ //Highlights the first selection
  function go(){
-        //Sets the previous selection to false (clearing the map from previous)
-        $('#Image-Maps-Com-image-maps-2015-08-13-174118').mapster('set',false, ''+current);
-
-       
+        $('area.'+current).mapster('set',false);
         var key = document.getElementById("mySelect").value;
-
-        $('#Image-Maps-Com-image-maps-2015-08-13-174118').mapster('set',true, ''+key);
+        $('area.'+key).mapster('set',true);
         current = key;
+        
      };
+
 
 //Highlight the selection for the second selection options
 function go2(){
-
-        $('#Image-Maps-Com-image-maps-2015-08-13-174118').mapster('set',false, ''+key);
-
-        var key = document.getElementById("mySelect").value;
-        $('#Image-Maps-Com-image-maps-2015-08-13-174118').mapster('set',false, ''+current);
-
-         key = document.getElementById("mySelect2").value;
-        $('#Image-Maps-Com-image-maps-2015-08-13-174118').mapster('set',true, ''+key);  
-        current = key;   
-        
-     }
+        $('area.'+current).mapster('set',false);
+        var key = document.getElementById("mySelect2").value;
+        $('area.'+key).mapster('set',true);  
+        current = key;       
+     };
